37a58b8487ab8640ddd669f2081b2df92f482d89
Jason Zeng <zengkaiting01@gmail.com>
2023-08-20 14:25:40 +1000

    Add Book Function

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 let Library = [];
+const showButton = document.getElementById("showDialog");
+const favDialog = document.getElementById("favDialog");
+const outputBox = document.querySelector("output");
+const bookNameInput = favDialog.querySelector("#bookNameInput");
+const authorInput = favDialog.querySelector("#authorInput");
+const pageInput = favDialog.querySelector("#pageInput");
+const readInput = favDialog.querySelector("#readInput");
+const confirmBtn = favDialog.querySelector("#confirmBtn");
 
 //Book object constructor
 function Book(uuid, title, author, pages, haveRead) {
@@ -8,6 +16,37 @@ function Book(uuid, title, author, pages, haveRead) {
   this.pages = pages;
   this.haveRead = haveRead;
 }
+
+//Function to add book to library
+function addBookToLibrary(uuid, title, author, pages, haveRead) {
+  Library.push(new Book(uuid, title, author, pages, haveRead));
+}
+
+//Function to remove book from library
+function removeBookFromLibrary(uuid) {
+  document.getElementById(uuid).remove();
+}
+
+// "Show the dialog" button opens the <dialog> modally
+showButton.addEventListener("click", () => {
+  favDialog.showModal();
+});
+
+// "Cancel" button closes the dialog without submitting because of [formmethod="dialog"], triggering a close event.
+favDialog.addEventListener("close", (e) => {
+  outputBox.value =
+    favDialog.returnValue === "default"
+      ? "No return value."
+      : `ReturnValue: ${favDialog.returnValue}.`; // Have to check for "default" rather than empty string
+});
+
+// Prevent the "confirm" button from the default behavior of submitting the form, and close the dialog with the `close()` method, which triggers the "close" event.
+confirmBtn.addEventListener("click", (event) => {
+  event.preventDefault(); // We don't want to submit this fake form
+  favDialog.close(); // Have to send the select box value here.
+  addBookToLibrary(self.crypto.randomUUID(), bookNameInput.value, authorInput.value, pageInput.value, readInput.value);
+  generateBookGrids();
+});
 
 //Generate Book Div
 function generateBookGrids() {
@@ -31,16 +70,6 @@ function generateBookGrids() {
       document.getElementById("d-"+Book.uuid).addEventListener("click", () =>{removeBookFromLibrary(Book.uuid)})
     });
   }  
-
-//Function to add book to library
-function addBookToLibrary(uuid, title, author, pages, haveRead) {
-  Library.push(new Book(uuid, title, author, pages, haveRead));
-}
-
-//Function to remove book from library
-function removeBookFromLibrary(uuid) {
-    document.getElementById(uuid).remove();
-}
 
 //Add some examples
 addBookToLibrary(self.crypto.randomUUID(), "Test1", "Author1", "203", true);
